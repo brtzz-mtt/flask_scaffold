d13be3be2d8a627a03b53c4a42a4e6c9af6f7461
brtzz-mtt <brtzz.mtt@gmail.com>
2021-11-13 11:58:39 +0100

    minor cli update

--- cli.sh
+++ cli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ $1 = "build" ] && [ $2 = "wget" ]; then
+    sh cli.sh test
     wget -k -K -E -r -l 10 -p -N -F --restrict-file-names=windows -nH http://127.0.0.1:5000/
 elif [ $1 = "test" ]; then
     coverage run -m --source=. pytest test.py -v
