69b134a12958b57e7154b70a2ec6da76a292e0e4
brtzz-mtt <brtzz.mtt@gmail.com>
2021-11-15 10:04:17 +0100

    maintenance

--- cli.sh
+++ cli.sh
@@ -4,7 +4,7 @@ if [ $1 = "build" ] && [ $2 = "wget" ]; then
     sh cli.sh test
     wget -k -K -E -r -l 10 -p -N -F --restrict-file-names=windows -nH http://127.0.0.1:5000/
 elif [ $1 = "test" ]; then
-    coverage run -m --source=. pytest test.py -v
+    coverage run -m --source=. pytest tests.py -v
     coverage html
     pipreqs --savepath requirements.txt
 fi
